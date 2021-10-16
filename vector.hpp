501ba234995a32ec3f7a7d1904906cec8f69b78d
saadelfadil <saadelfadil18@gmail.com>
2021-10-16 15:44:52 +0100

    Vector in progress

---
+++ vector.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+template <typename Vector>
+class VectorIterator
+{
+    private:
+        PointerType m_Ptr;
+    public:
+        using ValueType = typename Vector::ValueType;
+        using PointerType = ValueType*;
+        using ReferenceType = ValueType&;
+    public:
+        VectorIterator(PointerType ptr) : m_Ptr(ptr) {}
+
+
+        VectorIterator operator++()
+        {
+            m_Ptr++;
+            return *this;
+        }
+
+        VectorIterator operator++(int)
+        {
+            VectorIterator iterator = *this;
+            ++(*this);
+            return iterator;
+        }
+
+        VectorIterator operator--()
+        {
+            m_Ptr--;
+            return *this;
+        }
+
+        VectorIterator operator--(int)
+        {
+            VectorIterator iterator = *this;
+            --(*this);
+            return iterator;
+        }
+
+        ReferenceType operator[](int index)
+        {
+            return *(m_Ptr + index);
+        }
+
+        PointerType operator->()
+        {
+            return m_Ptr;
+        }
+
+        PointerType operator*()
+        {
+            return *m_Ptr;
+        }
+
+        bool operator==(const VectorIterator &other) const
+        {
+            return m_Ptr == other.m_Ptr;
+        }
+
+        bool operator!=(const VectorIterator &other) const
+        {
+            return !(m_Ptr == other.m_Ptr);
+        }
+}
