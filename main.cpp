667651058365ed7ed074c3975e4e4b8bd815c7a8
saadelfadil <saadelfadil18@gmail.com>
2021-10-20 17:47:18 +0100

    Vector in progress

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "vector.hpp"
+
+template <typename T>
+void Printvector(const Vector<T>& vector)
+{
+    for (size_t i = 0; i < vector.Size; i++)
+        std::cout << vector[i] << std::end;
+    std::cout << "------------------" << std::endl;
+}
+
+int main()
+{
+    Vector<std::string> vector;
+
+    vector.PushBack("Saad");
+    vector.PushBack("El fadil");
+    vector.PushBack("Agadir");
+
+    // std::cin.get();
+}
--- vector.hpp
+++ vector.hpp
@@ -1,34 +1,9 @@
 #pragma once
 #include <vector>
-#include <iostream>
 
 template <typename T>
 class Vector
 {
-    public:
-        Vector()
-        {
-            ReAlloc();
-        }
-
-        void pushBack(const T&value)
-        {
-            if (m_size >= m_Capacity)
-                ReAlloc(m_Capacity + (m_Capacity / 2));
-            m_data[m_Size] = value;
-            m_Size++;
-        }
-        
-        T&  operator[](size_t index)
-        {
-            return (m_data[index]);
-        }
-
-        size_t Size() const
-        {
-            return (this->m_Size);
-        }
-
     private:
         void ReAlloc(size_t newCapacity)
         {
@@ -45,44 +20,37 @@ class Vector
         T* m_data = nullptr;
 
         size_t m_Size = 0;
-        size_t m_Capacity = 0
+        size_t m_Capacity = 0;
+    public:
+        Vector()
+        {
+            ReAlloc(2);
+        }
+		
+        void PushBack(const T&value)
+        {
+            if (m_Size >= m_Capacity)
+                ReAlloc(m_Capacity + (m_Capacity / 2));
+            m_data[m_Size] = value;
+            m_Size++;
+        }
+
+        T&  operator=(T const &obj)
+        {
+            *this = obj;
+        }
+
+        T&  operator[](size_t index)
+        {
+            return (m_data[index]);
+        }
+
+        size_t Size() const
+        {
+            return (this->m_Size);
+        }
+
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -95,69 +63,69 @@ class Vector
 
 // iterator of vector
 
-template <typename Vector>
-class VectorIterator
-{
-    private:
-        PointerType m_Ptr;
+// template <typename Vector>
+// class VectorIterator
+// {
+//     private:
+//         PointerType m_Ptr;
         
-    public:
-        using ValueType = typename Vector::ValueType;
-        using PointerType = ValueType*;
-        using ReferenceType = ValueType&;
+//     public:
+//         using ValueType = typename Vector::ValueType;
+//         using PointerType = ValueType*;
+//         using ReferenceType = ValueType&;
 
-    public:
+//     public:
 
-        VectorIterator(PointerType ptr) : m_Ptr(ptr) {}
+//         VectorIterator(PointerType ptr) : m_Ptr(ptr) {}
 
-        VectorIterator operator++()
-        {
-            m_Ptr++;
-            return *this;
-        }
+//         VectorIterator operator++()
+//         {
+//             m_Ptr++;
+//             return *this;
+//         }
 
-        VectorIterator operator++(int)
-        {
-            VectorIterator iterator = *this;
-            ++(*this);
-            return iterator;
-        }
+//         VectorIterator operator++(int)
+//         {
+//             VectorIterator iterator = *this;
+//             ++(*this);
+//             return iterator;
+//         }
 
-        VectorIterator operator--()
-        {
-            m_Ptr--;
-            return *this;
-        }
+//         VectorIterator operator--()
+//         {
+//             m_Ptr--;
+//             return *this;
+//         }
 
-        VectorIterator operator--(int)
-        {
-            VectorIterator iterator = *this; 
-            --(*this);
-            return iterator;
-        }
+//         VectorIterator operator--(int)
+//         {
+//             VectorIterator iterator = *this; 
+//             --(*this);
+//             return iterator;
+//         }
 
-        ReferenceType operator[](int index)
-        {
-            return *(m_Ptr + index);
-        }
+//         ReferenceType operator[](int index)
+//         {
+//             return *(m_Ptr + index);
+//         }
 
-        PointerType operator->()
-        {
-            return m_Ptr;
-        }
+//         PointerType operator->()
+//         {
+//             return m_Ptr;
+//         }
 
-        PointerType operator*()
-        {
-            return *m_Ptr;
-        }
+//         PointerType operator*()
+//         {
+//             return *m_Ptr;
+//         }
 
-        bool operator==(const VectorIterator &other) const
-        {
-            return m_Ptr == other.m_Ptr;
-        }
+//         bool operator==(const VectorIterator &other) const
+//         {
+//             return m_Ptr == other.m_Ptr;
+//         }
 
-        bool operator!=(const VectorIterator &other) const
-        {
-            return !(m_Ptr == other.m_Ptr);
-        }
-};
+//         bool operator!=(const VectorIterator &other) const
+//         {
+//             return !(m_Ptr == other.m_Ptr);
+//         }
+// };
