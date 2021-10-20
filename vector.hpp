455108e026e67e0dff27bddcadf39e52d8f504a5
saadelfadil <saadelfadil18@gmail.com>
2021-10-20 17:31:44 +0100

    Vector in progress

--- vector.hpp
+++ vector.hpp
@@ -6,8 +6,46 @@ template <typename T>
 class Vector
 {
     public:
+        Vector()
+        {
+            ReAlloc();
+        }
+
+        void pushBack(const T&value)
+        {
+            if (m_size >= m_Capacity)
+                ReAlloc(m_Capacity + (m_Capacity / 2));
+            m_data[m_Size] = value;
+            m_Size++;
+        }
         
+        T&  operator[](size_t index)
+        {
+            return (m_data[index]);
+        }
+
+        size_t Size() const
+        {
+            return (this->m_Size);
+        }
+
     private:
+        void ReAlloc(size_t newCapacity)
+        {
+            T* newBlock = new T[newCapacity];
+            if (newCapacity < m_Size)
+                m_Size = newCapacity;
+            for (size_t i = 0; i < m_Size; i++)
+                newBlock[i] = m_data[i];
+            delete [] m_data;
+            m_data = newBlock;
+            m_Capacity = newCapacity;
+        }
+
+        T* m_data = nullptr;
+
+        size_t m_Size = 0;
+        size_t m_Capacity = 0
 };
 
 
