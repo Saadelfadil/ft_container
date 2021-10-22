299e5204708930b9ebe1395116ef74a2f582e2b1
saadelfadil <saadelfadil18@gmail.com>
2021-10-22 13:59:14 +0100

    Vector in progress

--- vector.hpp
+++ vector.hpp
@@ -4,21 +4,46 @@
 
 // #include <vector>
 
-template <typename T>
+template <typename T, class Allocator = std::allocator<T> >
 class Vector
 {
     private:
-        void ReAlloc(size_t newCapacity)
+        std::allocator<T> myallo;
+        // void ReAlloc(size_t newCapacity)
+        // {
+        //     T* newBlock = new T[newCapacity];
+        //     if (newCapacity < m_Size)
+        //         m_Size = newCapacity;
+        //     for (size_t i = 0; i < m_Size; i++)
+        //         newBlock[i] = m_data[i];
+        //     delete [] m_data;
+        //     m_data = newBlock;
+        //     m_Capacity = newCapacity;
+        // }
+
+        T* allocate_memory( std::size_t n)
         {
-            T* newBlock = new T[newCapacity];
-            if (newCapacity < m_Size)
-                m_Size = newCapacity;
+            T* newBlock = myallo.allocate(n);
+            if (n < m_Size)
+                m_Size = n;
             for (size_t i = 0; i < m_Size; i++)
                 newBlock[i] = m_data[i];
-            delete [] m_data;
+            // delete [] m_data;
             m_data = newBlock;
-            m_Capacity = newCapacity;
+            m_Capacity = n;
+            return m_data;
         }
+
+        void deallocate_memory(T* p, std::size_t n )
+        {
+            myallo.deallocate(p, n);
+        }
+
+        void destroy_memory(T* p)
+        {
+            p->~T();
+        }
+
     private:
         T* m_data; 
         size_t m_Size;
@@ -26,18 +51,21 @@ class Vector
     public:
         Vector() : m_data(nullptr), m_Size(0), m_Capacity(0)
         {
-            ReAlloc(2);
+            // ReAlloc(2);
+            allocate_memory(2);
         }
 
         ~Vector()
         {
-            delete[] m_data;
+            deallocate_memory(m_data, m_Capacity);
+            destroy_memory(m_data);
+            // delete[] m_data;
         }
-		
+
         void PushBack(const T&value)
         {
             if (m_Size >= m_Capacity)
-                ReAlloc(m_Capacity * 2);
+                allocate_memory(m_Capacity * 2);
             m_data[m_Size] = value;
             m_Size++;
         }
@@ -45,7 +73,7 @@ class Vector
         void insert(T item)
         {
             if (m_Size >= m_Capacity)
-                ReAlloc(m_Capacity * 2);
+                allocate_memory(m_Capacity * 2);
             m_data[m_Size++] = item;
         }
 
@@ -75,9 +103,17 @@ class Vector
             m_Size = 0;
         }
 
-        T&  operator=(T const &obj)
+        T&  operator=(Vector<T> const &obj)
         {
             *this = obj;
+        }
+
+        bool operator==(Vector<T> const &obj) const
+        {
+            for (std::size_t i = 0; i < m_Size; i++)
+                if (obj.array[i] != this->m_data[i])
+                    return false;
+            return true;
         }
 
         const T&  operator[](size_t index) const
