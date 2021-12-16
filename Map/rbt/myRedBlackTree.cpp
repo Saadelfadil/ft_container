e320e4ca4e508b6e635e29537865109611e70c73
sel-fadi <sel-fadi@student.1337.ma>
2021-12-16 19:33:51 +0100

    Starting map ( Red black tree (insertion done but without testing))

--- Map/rbt/myRedBlackTree.cpp
+++ Map/rbt/myRedBlackTree.cpp
@@ -6,7 +6,7 @@
 /*   By: sel-fadi <sel-fadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 15:22:33 by sel-fadi          #+#    #+#             */
-/*   Updated: 2021/12/14 23:30:00 by sel-fadi         ###   ########.fr       */
+/*   Updated: 2021/12/16 19:33:12 by sel-fadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,6 @@ namespace ft {
 		}
 		
 	}		RedBlack;
-	// typedef RedBlack *RedBlackNode;
 
 	class RedBlackTree
 	{
@@ -46,10 +45,120 @@ namespace ft {
 			{
 				this->root = nullptr;
 			}
+			
+			void rotateLeft(RedBlack *&root, RedBlack *&node)
+			{
+				RedBlack *node_right = node->right;
+			
+				node->right = node_right->left;
+			
+				if (node->right != NULL)
+					node->right->parent = node;
+			
+				node_right->parent = node->parent;
+			
+				if (node->parent == NULL)
+					root = node_right;
+				else if (node == node->parent->left)
+					node->parent->left = node_right;
+				else
+					node->parent->right = node_right;
+			
+				node_right->left = node;
+				node->parent = node_right;
+			}
+			
+			void rotateRight(RedBlack *&root, RedBlack *&node)
+			{
+				RedBlack *node_left = node->left;
+			
+				node->left = node_left->right;
+			
+				if (node->left != NULL)
+					node->left->parent = node;
+			
+				node_left->parent = node->parent;
+			
+				if (node->parent == NULL)
+					root = node_left;
+				else if (node == node->parent->left)
+					node->parent->left = node_left;
+				else
+					node->parent->right = node_left;
+			
+				node_left->right = node;
+				node->parent = node_left;
+			}
 
 			void fixViolation(RedBlack *root, RedBlack *newNode)
 			{
-				
+				RedBlack *parent_newNode = NULL;
+				RedBlack *grand_parent_newNode = NULL;
+
+				while ((newNode != root) && (newNode->color != BLACK) && (newNode->parent->color == RED))
+				{
+					parent_newNode = newNode->parent;
+					grand_parent_newNode = newNode->parent->parent;
+
+					/*  Case : A ---> Parent of newNode is left child of Grand-parent of newNode */
+					if (parent_newNode == grand_parent_newNode->left)
+					{
+						RedBlack *uncle_newNode = grand_parent_newNode->right;
+						/* Case : 1  The uncle of newNode is also red Only Recoloring required */
+						if (uncle_newNode != NULL && uncle_newNode->color == RED)
+						{
+							grand_parent_newNode->color = RED;
+							parent_newNode->color = BLACK;
+							uncle_newNode->color = BLACK;
+							newNode = grand_parent_newNode;
+						}
+						else
+						{
+							/* Case : 2  newNode is right child of its parent  Left-rotation required */
+							if (newNode == parent_newNode->right)
+							{
+								rotateLeft(root, parent_newNode);
+								newNode = parent_newNode;
+								parent_newNode = newNode->parent;
+							}
+							/* Case : 3  newNode is left child of its parent Right-rotation required */
+							rotateRight(root, grand_parent_newNode);
+							std::swap(parent_newNode->color, grand_parent_newNode->color);
+							newNode = parent_newNode;
+						}
+					}
+
+					/* Case : B  Parent of newNode is right child of Grand-parent of newNode */
+					else
+					{
+						 RedBlack *uncle_newNode = grand_parent_newNode->left;
+ 
+						/*  Case : 1 The uncle of newNode is also red Only Recoloring required */
+						if ((uncle_newNode != NULL) && (uncle_newNode->color == RED))
+						{
+							grand_parent_newNode->color = RED;
+							parent_newNode->color = BLACK;
+							uncle_newNode->color = BLACK;
+							newNode = grand_parent_newNode;
+						}
+						else
+						{
+							/* Case : 2 newNode is left child of its parent Right-rotation required */
+							if (newNode == parent_newNode->left)
+							{
+								rotateRight(root, parent_newNode);
+								newNode = parent_newNode;
+								parent_newNode = newNode->parent;
+							}
+			
+							/* Case : 3 newNode is right child of its parent Left-rotation required */
+							rotateLeft(root, grand_parent_newNode);
+							std::swap(parent_newNode->color, grand_parent_newNode->color);
+							newNode = parent_newNode;
+						}
+					}
+				}
+				root->color = BLACK;
 			}
 			
 			void insertion(value_type *val)
@@ -85,5 +194,25 @@ namespace ft {
 			}
 	};
 
+// 	int main() {
+// 		std::pair<int, int> value_type;
 
-}
+// 		ft::RedBlack RedBlack(std::pair<int, int> value_type);
+// 		RedBlackTree redblack;
+// 		redblack.insertion(55);
+// 		redblack.insertion(40);
+// 		redblack.insertion(65);
+// 		redblack.insertion(60);
+// 		redblack.insertion(75);
+// 		redblack.insertion(57);
+// 		redblack.insertion(56);
+// 		redblack.insertion(64);
+
+// 		redblack.printTree();
+// 		// cout << endl
+// 		//    << "After deleting" << endl;
+// 		// bst.deleteNode(40);
+// 		// bst.printTree();
+// 		}
+// }
+
--- Map/rbt/pair.hpp
+++ Map/rbt/pair.hpp
@@ -6,16 +6,22 @@
 /*   By: sel-fadi <sel-fadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 23:08:20 by sel-fadi          #+#    #+#             */
-/*   Updated: 2021/12/14 23:08:58 by sel-fadi         ###   ########.fr       */
+/*   Updated: 2021/12/16 19:17:51 by sel-fadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#pragma once
+
+# include <iostream>
+# include <stdlib.h>
+# include <string>
 
 namespace ft
 {
 	template < class T1, class T2 > struct pair {
 		public:
-			T1 first;	
-			T2 second;
+			typedef T1 first_type;	
+			typedef T2 second_type;
 			pair() : first(), second() {}
 			pair ( const pair<T1, T2>& pai ) : first(pai.first), second(pai.second) {}
 
@@ -32,35 +38,38 @@ namespace ft
 			}
 			~pair() {}
 
-			template <class T1, class T2>
-			bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
-			{ return lhs.first==rhs.first && lhs.second==rhs.second; }
-
-			template <class T1, class T2>
-			bool operator!= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
-			{ return !(lhs==rhs); }
-
-			template <class T1, class T2>
-			bool operator<  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
-			{ return lhs.first<rhs.first || (!(rhs.first<lhs.first) && lhs.second<rhs.second); }
-
-			template <class T1, class T2>
-			bool operator<= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
-			{ return !(rhs<lhs); }
-
-			template <class T1, class T2>
-			bool operator>  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
-			{ return rhs<lhs; }
-
-			template <class T1, class T2>
-			bool operator>= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
-			{ return !(lhs<rhs); }
+			first_type first;
+			second_type second;
 	};
 
-	template <class T1,class T2>
-	pair<T1,T2> make_pair (T1 x, T2 y)
-	{
-		return ( pair<T1,T2>(x,y) );
-	}
+	// template <class T1,class T2>
+	// pair<T1,T2> make_pair (T1 x, T2 y)
+	// {
+	// 	return ( pair<T1,T2>(x,y) );
+	// }
+	
+	template <class T1, class T2>
+	bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return lhs.first==rhs.first && lhs.second==rhs.second; }
+
+	template <class T1, class T2>
+	bool operator!= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(lhs==rhs); }
+
+	template <class T1, class T2>
+	bool operator<  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return lhs.first<rhs.first || (!(rhs.first<lhs.first) && lhs.second<rhs.second); }
+
+	template <class T1, class T2>
+	bool operator<= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(rhs<lhs); }
+
+	template <class T1, class T2>
+	bool operator>  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return rhs<lhs; }
+
+	template <class T1, class T2>
+	bool operator>= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(lhs<rhs); }
 
 }
